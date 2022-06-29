28de969d9713ee22a0a249f460e4b1d7f8299fb4
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-06-28 19:08:17 -0500

    Finalizado version1

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { PropTypes } from "prop-types";
 
 export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState("");
@@ -18,13 +19,18 @@ export const AddCategory = ({ onNewCategory }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit = {onSubmit} aria-label = "form">
       <input
         type='text'
         placeholder='Buscar Gifs'
         value={inputValue}
         onChange={oninputChange}
       />
+      <button type='submit'>Buscar</button>
     </form>
   );
 };
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
+}
--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,28 +1,25 @@
 import { render } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
-describe('Prueba en <GifItem />', () => {
+describe("Prueba en <GifItem />", () => {
+  const title = "Goku";
+  const url = "https://media.giphy.com/media/CY3A9zOlZR8uhFbeok/giphy.gif";
 
-    const title = 'Goku';
-    const url = 'https://media.giphy.com/media/CY3A9zOlZR8uhFbeok/giphy.gif';
+  test("Debe hacer match con el snapshot", () => {
+    const { container } = render(<GifItem title={title} url={url} />);
+    expect(container).toMatchSnapshot();
+  });
 
-    test('Debe hacer match con el snapshot', () => {
-        const {container} = render(<GifItem title={title} url={url} />);
-        expect(container).toMatchSnapshot();
-    });
+  test("Debe de mostrar la imagen con el Url y el alt indicado", () => {
+    render(<GifItem title={title} url={url} />);
 
-    test('Debe de mostrar la imagen con el Url y el alt indicado', () => {
-        render(<GifItem title={title} url={url} />);
+    const { src, alt } = document.querySelector("img");
+    expect(src).toBe(url);
+    expect(alt).toBe(alt);
+  });
 
-        const { src, alt } = document.querySelector('img');
-        expect(src).toBe(url);
-        expect(alt).toBe(alt);
-    }
-    );
-
-    test('Debe de mostrar el titulo indicado', () => {
-        render(<GifItem title={title} url={url} />);
-        expect(document.querySelector('p').textContent).toBe(title);
-    }
-    );
+  test("Debe de mostrar el titulo indicado", () => {
+    render(<GifItem title={title} url={url} />);
+    expect(document.querySelector("p").textContent).toBe(title);
+  });
 });
